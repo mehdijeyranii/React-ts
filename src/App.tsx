0732306b8e688b3f911e164e01f62db3a1f9ce1d
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-11 10:51:40 +0330

    Use Greeting component inside App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Greeting from "./components/Greeting";
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Welcome to React + TypeScrypt</h1>
-    </>
+      <Greeting name="Ali" />
+      <Greeting name="Mehdi" />
+    </div>
   );
 }
 
