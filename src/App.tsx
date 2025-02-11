49292c9381e1518a242828010995b8210af52b00
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-11 14:27:30 +0330

    Use TodoList component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import TodoList from "./components/TodoList";
+
+const todos = [
+  { task: "Learn React", completed: true },
+  { task: "Practice TypeScript", completed: false },
+  { task: "Build a project", completed: false },
+];
 
 function App() {
   return (
     <div>
-      <h1>Welcome to React + TypeScrypt</h1>
+      <h1>Welcome to React + TypeScript</h1>
+      <h2>My To-Do List</h2>
+      <TodoList todos={todos} />
     </div>
   );
 }
