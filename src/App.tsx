d7f9692c43b9ae34fe007bba88af48dd1dbbe7db
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-11 18:44:39 +0330

    Use Button component with different colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Button from "./components/button/Button";
 import TodoList from "./components/TodoList";
 
 const todos = [
@@ -13,6 +14,11 @@ function App() {
       <h1>Welcome to React + TypeScript</h1>
       <h2>My To-Do List</h2>
       <TodoList todos={todos} />
+      <hr />
+      <h2>Styled Buttons</h2>
+      <Button text="Click Me" color="primary" />
+      <Button text="Cancel" color="secondary" />
+      <Button text="Delete" color="danger" />
     </div>
   );
 }
