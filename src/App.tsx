e93afa439b3d541e835af5832f7caeb01511330d
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-11 11:45:32 +0330

    Use ProfileCard component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import ProfileCard from "./components/ProfileCard";
+import ImageUser from "./assets/01.avif";
 
 function App() {
   return (
     <div>
       <h1>Welcome to React + TypeScrypt</h1>
+      <h2>User Profiles</h2>
+      <ProfileCard name="Ali" role="Frontend Developer" imageUrl={ImageUser} />
+      <ProfileCard name="Sara" role="UI/UX Designer" imageUrl={ImageUser} />
     </div>
   );
 }
